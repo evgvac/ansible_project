9c68042d4c11d673509636a17d94f31e646223b3
Vachenko Evgenii Nikolaevich <evgenii.vachenko@kronshtadt.ru>
2022-08-24 09:40:36 +0300

    change exec

--- debian_tuning/default_settings/cisco/exec.php
+++ debian_tuning/default_settings/cisco/exec.php
@@ -1,32 +1,32 @@
 <html>
 <head>
-	<title>Command execution</title>
+        <title>Command execution</title>
 </head>
 <body>
 <pre>
 <?php
-	$CMD = 'ansible-playbook -l {IPADDR} /etc/ansible/playbooks/cisco/clear_port-security.yml -vvvv';
-	if (! array_key_exists('ipaddr', $_POST)) {
-		echo 'IP-address for using in command is not received';
-		exit;
-	}
-	$ipaddr = $_POST[ 'ipaddr' ];
+        $CMD = '/usr/bin/ansible-playbook -l {IPADDR} /etc/ansible/playbooks/cisco/clear_port-security.yml --user someuser --key-file "/etc/ansible/.ssh/port_security.ppk"';
+        if (! array_key_exists('ipaddr', $_POST)) {
+                echo 'IP-address for using in command is not received';
+                exit;
+        }
+        $ipaddr = $_POST[ 'ipaddr' ];
 
-	$exe = str_replace('{IPADDR}', $ipaddr, $CMD);
+        $exe = str_replace('{IPADDR}', $ipaddr, $CMD);
 
-	print('Executing command: \'' . $exe . '\'');
-	$result = 0;
-	$out = '';
-	exec($exe, $out, $result);
+        print('Executing command: \'' . command . '\'');
+        $result = 0;
+        $out = '';
+        exec($exe, $out, $result);
 
-	if ($result === 0)
-		echo ' - OK!' . "\n\n";
-	else {
-		echo ' - Error';
-		exit;
-	}
-	foreach ($out as $l)
-		print $l . "\n";
+        if ($result === 0)
+                echo ' - OK!' . "\n\n";
+        else {
+                echo ' - Error';
+                exit;
+        }
+        foreach ($out as $l)
+                print $l . "\n";
 ?>
 </pre>
 </body>
