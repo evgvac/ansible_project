665e02f7c395c02dda5b65b76a7ff91064813bb9
Vachenko Evgenii Nikolaevich <evgenii.vachenko@kronshtadt.ru>
2023-01-18 10:32:07 +0300

    add folder php

---
+++ php/exec.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+        <title>Command execution</title>
+</head>
+<body>
+<pre>
+<?php
+        $CMD = '/usr/bin/ansible-playbook -l {IPADDR} /etc/ansible/playbooks/cisco/clear_port-security.yml --key-file "/etc/ansible/.ssh/private.key"';
+        if (! array_key_exists('ipaddr', $_POST)) {
+                echo 'IP-address for using in command is not received';
+                exit;
+        }
+        $ipaddr = $_POST[ 'ipaddr' ];
+
+        $exe = str_replace('{IPADDR}', $ipaddr, $CMD);
+
+        print('Executing command: \'' . command . '\'');
+        $result = 0;
+        $out = '';
+        exec($exe, $out, $result);
+
+        if ($result === 0)
+                echo ' - OK!' . "\n\n";
+        else {
+                echo ' - Error';
+                exit;
+        }
+        foreach ($out as $l)
+                print $l . "\n";
+?>
+</pre>
+</body>
+</html>
